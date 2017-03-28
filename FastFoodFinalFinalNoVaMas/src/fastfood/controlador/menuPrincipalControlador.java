2e9a28391a1589b9fb17065c1251077ed32cd35b
Steven Anaya <stvanaya@hotmail.com>
2017-03-28 12:53:09 -0500

    Creacion del paquete modelo, creacion de los getter y setter de la clase login y de la clase menuPrincipalControlador

---
+++ FastFoodFinalFinalNoVaMas/src/fastfood/controlador/menuPrincipalControlador.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fastfood.controlador;
+import fastfood.modelo.modelo;
+import fastfood.vista.*;
+
+/**
+ *
+ * @author COlo
+ */
+public class menuPrincipalControlador {
+    
+    private String nombre;
+    
+    public menuPrincipalControlador(){
+
+    
+    }
+
+    /*------------------------------------METODOS GETTERS Y SETTERS--------------------------------------------*/
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    /*-----------------------------------------------------------------------------------------------------------*/
+    
+    public void ConectarModelo(){
+    
+        modelo obj = new modelo();
+        obj.capturaNombre();
+    
+    
+    }
+    
+}
